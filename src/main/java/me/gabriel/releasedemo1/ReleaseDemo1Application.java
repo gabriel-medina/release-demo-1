45f8812c7500051ad1423b9d6d33b205bd9959d4
Gabriel Medina <gabriel.medina@globant.com>
2018-08-06 16:32:19 -0500

    First commit

---
+++ src/main/java/me/gabriel/releasedemo1/ReleaseDemo1Application.java
@@ -0,0 +1,12 @@
+package me.gabriel.releasedemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseDemo1Application.class, args);
+	}
+}
